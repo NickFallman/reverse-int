2369b774095286da32c20adcdf3f7ae355c5b8fb
Valentine Garbuzenko <nickf@tut.by>
2022-02-09 21:57:54 +0300

    feat: add debug version of reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function reverse (n) {
-  
+const intNum = -123;    // -321
+
+// module.exports = 
+function reverse (n) {
+  let resInt = n;
+  console.log(resInt);
+  resInt = parseInt(
+    resInt
+      .toString() // start conveyor. converts the given Int number into a String Num = > 'Num'
+      .split('')  // converts the String into an Array of characters 'Num' => ['dig1'(msd), 'dig2', ... , digN(lsd)]
+      .reverse()  // reverses the order of the items in the array ['dig1'(msd), 'dig2', ... , digN(lsd)] => ['digN'(lsd), 'digN-1', ... , dig1(msd)]
+      .join('')   // reassembles the reversed characters into a String ['digN'(lsd), 'digN-1', ... , dig1(msd)] => 'revNum'
+  ) * Math.sign(n);
+  console.log(resInt);
+  return (resInt);                 
 }
+
+const result = reverse (intNum);
+// console.log(result);
